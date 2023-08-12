582ad4085aa38fb72caf45fb3b49f00fef5842fd
Shadi Ghaith <s_gheith@hotmail.com>
2023-08-12 13:08:02 +0300

    lesson-4

--- controllers/youTubeInsightsController.js
+++ controllers/youTubeInsightsController.js
@@ -6,122 +6,202 @@ const openai = require('../config/openaiConfig')
 
 const genarateYouTubeInsights = async (link, not_english) => {
 
-    const filePath = await youtubeAudioDownloader(link)
+  const filePath = await youtubeAudioDownloader(link)
 
-    console.log(filePath)
+  //console.log(filePath)
 
-    const transcriptFilename = await generateTranscription(filePath, not_english)
+  const transcriptFilename = await generateTranscription(filePath, not_english)
 
-    console.log(transcriptFilename)
+  //console.log(transcriptFilename)
+
+  //transcriptFilename = 'transcript-Do We Have Free Will  Robert Sapolsky & Andrew Huberman.txt'
+  //transcriptFilename = 'transcript-Three-Layer Architecture Pattern  Spring Boot Application Three-Layer Architecture.txt'
+  const insights = await generateInsights(transcriptFilename)
+
+  //console.log(insights)
+
+  return insights
 }
 
 const youtubeAudioDownloader = async (link) => {
 
-    // Verify a valid youtube link
-    if (!link.includes('youtube.com')) {
-        console.log('Invalid YouTube link!');
-        return false;
-    }
+  // Verify a valid youtube link
+  if (!link.includes('youtube.com')) {
+    console.log('Invalid YouTube link!');
+    return false;
+  }
 
-    try {
+  try {
 
 
-        // Obtain video information
-        const info = await ytdl.getInfo(link);
+    // Obtain video information
+    const info = await ytdl.getInfo(link);
 
-        // Just download the audio
-        const audioFormat = ytdl.chooseFormat(info.formats, { filter: 'audioonly' });
+    // Just download the audio
+    const audioFormat = ytdl.chooseFormat(info.formats, { filter: 'audioonly' });
 
-        // Save as <video_title>.mp3, sanitize to make sure the title doesn't have illegal characters
-        const outputFilePath = `./${sanitize(info.videoDetails.title)}.mp3`;
-        
-        console.log('Downloading the audio stream ...');
+    // Save as <video_title>.mp3, sanitize to make sure the title doesn't have illegal characters
+    const outputFilePath = `./${sanitize(info.videoDetails.title)}.mp3`;
 
-        // Create download streem
-        const stream = ytdl.downloadFromInfo(info, { filter: 'audioonly' });
+    console.log('Downloading the audio stream ...');
 
-        // Open output file stream
-        const writeStream = fs.createWriteStream(outputFilePath);
+    // Create download streem
+    const stream = ytdl.downloadFromInfo(info, { filter: 'audioonly' });
 
-        // Connect the two streams
-        stream.pipe(writeStream);
+    // Open output file stream
+    const writeStream = fs.createWriteStream(outputFilePath);
 
-        // Create a new Promise function to listen on the finish and error events
-        return new Promise((resolve, reject) => {
+    // Connect the two streams
+    stream.pipe(writeStream);
 
-            // Listen on finish event 
-            writeStream.on('finish', () => {
+    // Create a new Promise function to listen on the finish and error events
+    return new Promise((resolve, reject) => {
 
-                console.log('\nDownload complete!');
+      // Listen on finish event 
+      writeStream.on('finish', () => {
 
-                // Promised resolved (success)
-                resolve(outputFilePath);
-            });
+        console.log('\nDownload complete!');
 
-            // Listen on finish event 
-            writeStream.on('error', (error) => {
+        // Promised resolved (success)
+        resolve(outputFilePath);
+      });
 
-                console.error('Error downloading the file!', error);
+      // Listen on finish event 
+      writeStream.on('error', (error) => {
 
-                // Promised rejected (error)
-                reject(error);
-            });
+        console.error('Error downloading the file!', error);
 
-        });
+        // Promised rejected (error)
+        reject(error);
+      });
 
-    } catch (error) {
-        console.error('Error:', error.message);
-        return false;
-    }
+    });
+
+  } catch (error) {
+    console.error('Error:', error.message);
+    return false;
+  }
 }
 
 const generateTranscription = async (audio_file, not_english = false) => {
 
-    // Verify the audio file exists
-    if (!fileExists(audio_file)) {
-      console.log('Audio file does not exist!');
-      return
-    }
-  
-    transcript = ''
-  
-    // Translate or Transcript
-    if (not_english) {
-      transcript = await openai.createTranslation(
-        fs.createReadStream(audio_file),
-        "whisper-1"
-      );
-    }
-    else {
-      transcript = await openai.createTranscription(
-        fs.createReadStream(audio_file),
-        "whisper-1"
-      );
-    }
-  
-    console.log(transcript.data.text)
-  
-    const { name, ext } = path.parse(audio_file);
-    const transcriptFilename = `transcript-${name}.txt`;
-    const transcriptText = transcript.data.text;
-  
-    try {
-      fs.writeFileSync(transcriptFilename, transcriptText);
-      console.log('Transcript file written successfully!');
-    } catch (error) {
-      console.error('Error writing transcript file:', error);
-    }
-  
-    return transcriptFilename
+  // Verify the audio file exists
+  if (!fileExists(audio_file)) {
+    console.log('Audio file does not exist!');
+    return
   }
 
-  function fileExists(filePath) {
-    try {
-      fs.accessSync(filePath, fs.constants.F_OK);
-      return true;
-    } catch (error) {
-      return false;
-    }
+  transcript = ''
+
+  // Translate or Transcript
+  if (not_english) {
+    transcript = await openai.createTranslation(
+      fs.createReadStream(audio_file),
+      "whisper-1"
+    );
   }
+  else {
+    transcript = await openai.createTranscription(
+      fs.createReadStream(audio_file),
+      "whisper-1"
+    );
+  }
+
+  //console.log(transcript.data.text)
+
+  const { name, ext } = path.parse(audio_file);
+  const transcriptFilename = `transcript-${name}.txt`;
+  const transcriptText = transcript.data.text;
+
+  try {
+    fs.writeFileSync(transcriptFilename, transcriptText);
+    console.log('Transcript file written successfully!');
+  } catch (error) {
+    console.error('Error writing transcript file:', error);
+  }
+
+  return transcriptFilename
+}
+
+const generateInsights = async (transcript_filename) => {
+
+  // Validates Transcript file exists
+  if (!fileExists(transcript_filename)) {
+    console.log('Transcript file does not exist!');
+    return
+  }
+
+  let transcript_text = '';
+
+  // Read transcript text from the file
+  try {
+
+    const data = fs.readFileSync(transcript_filename, 'utf8');
+
+    transcript_text = data.toString();
+
+  } catch (error) {
+
+    console.error('Error reading file:', error);
+
+    return
+  }
+
+  system_prompt = `You are the insightful AI assistant. 
+                      Please analyze the following YouTube content and provide important keywords and main topics covered.
+
+                      YouTube Content: ${transcript_text}
+    `
+
+  prompt = `Please generate the output EXACTLY in this JSON format:
+                      {
+                          "keywords": [
+                              "First Keyword",
+                      …
+                              "Last Keyword"
+                          ],
+                          "topics": [
+                              {
+                                  "title": "First Topic Title",
+                                  "description": "First Topic Description"
+                              },
+                      …
+                              {
+                                  "title": "Last Topic Title",
+                                  "description": "Last Topic Description"
+                              }
+                          ]
+                      }
+    `
+
+  //console.log(prompt)
+
+  messages = [
+    { 'role': 'system', 'content': system_prompt },
+    { 'role': 'user', 'content': prompt }
+  ]
+
+  const response = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: messages,
+    temperature: 1,
+    max_tokens: 2048,
+  })
+
+  console.log(response.data.choices[0].message.content)
+
+  insights = response.data.choices[0].message.content
+
+  return JSON.parse(insights)
+}
+
+function fileExists(filePath) {
+  try {
+    fs.accessSync(filePath, fs.constants.F_OK);
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
 
 module.exports = { genarateYouTubeInsights }
--- playground.js
+++ playground.js
@@ -1,8 +1,10 @@
 
 const { genarateYouTubeInsights } = require('./controllers/youTubeInsightsController');
 
-link = 'https://www.youtube.com/watch?v=RI3JCq9-bbM'
+//link = 'https://www.youtube.com/watch?v=RI3JCq9-bbM'
 //link = 'https://www.youtube.com/watch?v=lKfhVLqadDQ' // Marie Curie in French
 //link = 'https://www.youtube.com/watch?v=WrhVdCsZHTE'  // Mahmoud Darwish in Arabic
+link = 'https://www.youtube.com/watch?v=ePsLTBvpnD8' // Three-Layer Architecture Pattern
 
-genarateYouTubeInsights(link, false)
+genarateYouTubeInsights(link, false).then(insights => console.log(insights))
+
